3fda78f0ae75fca24112a0176db3028b88447b00
Alejandro Armas <lxarmas@gmail.com>
2023-07-31 16:41:09 +0300

    addImages

--- index.js
+++ index.js
@@ -52,3 +52,26 @@ document.getElementById("newsletter-form").addEventListener("submit", function (
   const email = event.target.elements.email.value;
   // Add your newsletter subscription logic here (e.g., using AJAX)
 });
+
+
+// SERVICES 
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
