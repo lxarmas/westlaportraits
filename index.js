f88fc1c92bdbcf9c5c77254339fc6771d9dc1f9c
Alejandro Armas <lxarmas@gmail.com>
2023-10-03 11:57:28 -0700

    bootstrapcarousel

--- index.js
+++ index.js
@@ -61,33 +61,4 @@ function four() {
   document.getElementsByClassName("container")[0].classList.add("four");
 }
 
-// TEXT FOR ABOUT 
-// JavaScript for animations and form submission (if needed)
-document.getElementById("newsletter-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  const email = event.target.elements.email.value;
-  // Add your newsletter subscription logic here (e.g., using AJAX)
-});
 
-
-// SERVICES 
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let  i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
