ca45b661622599004ad764089fc0fdf04668c3d9
Alejandro Armas <lxarmas@gmail.com>
2023-10-07 11:46:17 -0700

    test5

--- index.js
+++ index.js
@@ -33,4 +33,22 @@ $(document).ready(function () {
   $('#carouselExampleAutoplaying').carousel();
 });
 
+// for about animation 
 
+function typeText(elementId, text, speed) {
+  const target = document.getElementById(elementId);
+  let index = 0;
+  function type() {
+    if (index < text.length) {
+      target.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(type, speed);
+    }
+  }
+  type();
+}
+
+typeText("line1", "An International Photographer", 100);
+setTimeout(() => {
+  typeText("line2", "with a Passion for Capturing Timeless Moments", 100);
+},9000); // Delay the second line typing animation
