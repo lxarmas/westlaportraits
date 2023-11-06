bd730067f00c4bde29b28904aad0fc9adee92ebc
Alejandro Armas <lxarmas@gmail.com>
2023-11-06 10:03:44 -0800

    carousel_cursors_fixed_mobile

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 
 
-// GRID PICTURE FOR HOMEPAGE
-let elements = document.getElementsByClassName("column");
-
-// Declare a "loop" variable
-let i;
 
 
 
-// Three  images side by side
-function two() {
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "33.33%";
-  }
-  document.getElementsByClassName("container")[0].classList.add("two");
-}
+
 
 
 // for about animation 
