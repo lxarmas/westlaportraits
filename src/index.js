354d03bef12e2e8ad8a487c44268af76fca2f82f
Alejandro Armas <lxarmas@gmail.com>
2025-10-11 10:25:22 -0700

    testing_gallery

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './tailwind.css';
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
 
       <App />
 
